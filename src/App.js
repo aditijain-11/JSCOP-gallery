13b769ff19e4a08f153d7ef2716bb1201c06134c
aditi <aditijain1100@gmail.com>
2022-03-09 11:27:46 +0530

    JSCOP-GALLERY

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Yearwise from "./components/Yearwise/Yearwise";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Gallery</p>
+      <Yearwise year="2018" imageLink="https://randomwordgenerator.com/img/picture-generator/55e0d0434c53a914f1dc8460962e33791c3ad6e04e507440722d72d29245c5_640.jpg" />
+    </div>
+
+
+
+  );
+}
+
+
+export default App;
